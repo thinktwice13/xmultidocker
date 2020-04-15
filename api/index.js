67e056eb7d285195877bc1b260d5416e9f3d79bf
thinktwice13 <marioo.krajacic@gmail.com>
2020-04-15 13:13:56 +0200

    Add error labels to index.js

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ pgClient.on("error", () => console.log("Lost PG connection"));
 
 pgClient
   .query("CREATE TABLE IF NOT EXISTS values (number INT)")
-  .catch((err) => console.log(err));
+  .catch((err) => console.log({ MY_ERROR: err }));
 
 // Redis Client Setup
 const redis = require("redis");
@@ -47,6 +47,7 @@ app.get("/values/all", async (req, res) => {
 
 app.get("/values/current", async (req, res) => {
   redisClient.hgetall("values", (err, values) => {
+    console.log({ MY_ERROR: err, values });
     res.send(values);
   });
 });
